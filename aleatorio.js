9c642049bdb6c045905fa86094e79e7f73eb3907
Ilanagalon <107581286+Ilanagalon@users.noreply.github.com>
2024-11-08 10:48:00 -0300

    08/11

--- aleatorio.js
+++ aleatorio.js
@@ -1,4 +1,10 @@
+const nomes = ["Larissa", "Julia", "Joao", "Isabella", "Diogo", "Sonia", "Manuela", "Amanda", "Diego", "Ilana"];
+
+
+
 export function aleatorio(lista){
     const posicao = Math.floor(Math.random()*lista.length)
     return lista [posicao];
 }
+
+export const nome = aleatorio (nomes)
